a8cf7d6dd1d8abb299135440a6f85f375895a754
lewster7 <105780974+lewster7@users.noreply.github.com>
2022-05-18 17:08:57 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+if (isset($_GET['q'])):
+            $q = ($_GET['q']);
+            
+function http_request($url){
+$userAgent = array(
+'Mozilla/5.0 (X11; Linux i686) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5',
+'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
+'Opera/9.25 (Windows NT 5.1; U; en)',
+'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
+'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
+'Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:50',
+'Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile Safari/534.1+2011-10-16 20:21:10');
+    // persiapkan curl
+    $ch = curl_init(); 
+
+    // set url 
+    curl_setopt($ch, CURLOPT_URL, $url);
+    // set user agent    
+    curl_setopt($ch, CURLOPT_USERAGENT, $userAgent[array_rand($userAgent)]);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    // return the transfer as a string 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+    // $output contains the output string 
+    $output = curl_exec($ch); 
+
+    // tutup curl 
+    curl_close($ch);      
+
+    // mengembalikan hasil curl
+    return $output;
+}
+
+$Rev = http_request("https://sonar.omnisint.io/reverse/q");
+
+$Revi = json_decode($Rev, true);
+
+foreach($Revi as $Revip){
+
+echo "".$Revip."\n";
+
+}
+endif;
+?>
